4603f61811de5314d7f185df6ab47c6503701b8c
Sergio Ratti <sratti@hotmail.co.uk>
2016-02-05 19:17:40 +0000

    create new template

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   get 'about', to: 'pages#about'
+  resources :articles
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
