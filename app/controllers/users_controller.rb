cb92c725c6d47b7a42d115ece428eae9ee15f7e1
Sergio Ratti <sratti@hotmail.co.uk>
2016-02-24 19:55:41 +0000

    add user listing and updated article view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,6 +33,10 @@ class UsersController < ApplicationController
       @user = User.find(params[:id]) 
    end
    
+   def index
+      @users = User.all 
+   end
+   
    private
    
    def user_params
